c1f447c38cebc27917ff01f5786c31db718d1222
dhruvil savaliya <20it124@charusat.edu.in>
2022-09-14 13:48:18 +0530

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavigationBar/>
       <Student/>
       <StudentList/>
+      <Footer/>
     </div>
   );
 }
